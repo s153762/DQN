7fc0ec569e71b6031d0325328c93eab074be0001
s153762 <s153762@student.dtu.dk>
2019-11-25 15:50:11 +0100

    test run

--- pong/script/script.sh
+++ pong/script/script.sh
@@ -3,7 +3,7 @@
 #BSUB -gpu "num=1"
 #BSUB -J myJob
 #BSUB -n 1
-#BSUB -W 10:00
+#BSUB -W 01:00
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -o logs/%J.out
 #BSUB -e logs/%J.err
@@ -13,4 +13,4 @@
 
 echo "Running script..."
 python3 Pong_DDQN.py
-echo "Script.fishished."
+echo "Script fishished."

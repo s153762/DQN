a318eec38dc048f25e9146a6455799e2b3483587
s153762 <s153762@student.dtu,dk>
2019-11-27 22:22:57 +0100

    new pong script

---
+++ pong/script_pong/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#BSUB -q gpuk80
+#BSUB -gpu "num=1"
+#BSUB -J myJob
+#BSUB -n 1
+#BSUB -W 20:00
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -o ../logs/%J.out
+#BSUB -e ../logs/%J.err
+#BSUB -u eida@dtu.dk
+#BSUB -B
+#BSUB -N
+
+echo "Running script..."
+python3 Pong_DDQN.py
+echo "Script fishished."

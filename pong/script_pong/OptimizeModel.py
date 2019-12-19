f9d54cb407dbe60715a2ebe92a82e302e319f9a5
s153762 <s153762@student.dtu,dk>
2019-12-19 16:16:13 +0100

    corrected OptimizeModel

--- pong/script_pong/OptimizeModel.py
+++ pong/script_pong/OptimizeModel.py
@@ -1,7 +1,20 @@
 import torch
 import torch.nn.functional as F
-from ReplayMemory import sample_memory
 from torch import nn
+
+def sample_memory(memory, BATCH_SIZE, Transition, device, non_blocking=False):
+    # sample replay buffer
+    transitions = memory.sample(BATCH_SIZE)
+    batch = Transition(*zip(*transitions))
+
+    # convert to tensors and create batches
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None]).to(device, non_blocking=non_blocking)
+    state_batch = torch.cat(batch.state).to(device, non_blocking=non_blocking)
+    action_batch = torch.cat(batch.action).to(device, non_blocking=non_blocking)
+    reward_batch = torch.cat(batch.reward).to(device, non_blocking=non_blocking)
+
+    return state_batch, action_batch, reward_batch, non_final_mask, non_final_next_states
 
 
 def optimize_model(memory, BATCH_SIZE, Transition, device, policy_net, target_net, GAMMA, optimizer, batch=None):
@@ -34,6 +47,6 @@ def optimize_model(memory, BATCH_SIZE, Transition, device, policy_net, target_ne
     loss.backward()
     nn.utils.clip_grad_norm_(policy_net.parameters(), 10.)
     optimizer.step()
-    return loss
+    return loss.clone()
 
 
